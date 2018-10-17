fd715997dffb710afafb0f6e9c612ea8bab17257
hanyuanliang <hanyuanliang@hoolai.com>
2018-10-17 19:52:18 +0800

    ribbon load balance

---
+++ ribbon-loadbalance/src/main/java/com/github/zerowise/RibbonLoadbalanceApplication.java
@@ -0,0 +1,36 @@
+package com.github.zerowise;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class RibbonLoadbalanceApplication {
+
+    @Autowired
+    private EurekaService eurekaService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonLoadbalanceApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/")
+    public String home(){
+        return eurekaService.getEurekaContent();
+    }
+}
