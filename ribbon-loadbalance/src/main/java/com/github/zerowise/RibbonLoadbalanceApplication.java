b964f98c1f1ddb4c46cfce36111cb02a5f7bfbeb
hanyuanliang <hanyuanliang@hoolai.com>
2018-10-17 20:27:48 +0800

    ribbon/fegin use hystrix Dashboard

--- ribbon-loadbalance/src/main/java/com/github/zerowise/RibbonLoadbalanceApplication.java
+++ ribbon-loadbalance/src/main/java/com/github/zerowise/RibbonLoadbalanceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @RestController
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class RibbonLoadbalanceApplication {
 
     @Autowired
