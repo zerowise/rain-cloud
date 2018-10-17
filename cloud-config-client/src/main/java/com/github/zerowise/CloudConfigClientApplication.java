7b2cedf8aa93a02bb7b71178d47476797a2ad605
hanyuanliang <hanyuanliang@hoolai.com>
2018-10-17 17:26:54 +0800

    fetch config server from eureka server

--- cloud-config-client/src/main/java/com/github/zerowise/CloudConfigClientApplication.java
+++ cloud-config-client/src/main/java/com/github/zerowise/CloudConfigClientApplication.java
@@ -3,12 +3,15 @@ package com.github.zerowise;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
+@EnableEurekaClient
 public class CloudConfigClientApplication {
     @Value("${book}")
     private String bookName;
